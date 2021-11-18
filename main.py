c08a10f94edfc521f20bde7cd4df09f7f410b84c
Kraystone <45125727+Kraystone@users.noreply.github.com>
2021-11-18 11:12:23 +0100

    [MODIFY] add new test file with functional test suite

--- main.py
+++ main.py
@@ -5,14 +5,4 @@ def CAPITALIZE(chaine):
     return chaine.upper()
 
 
-class Test(unittest.TestCase):
-    def CAPITALIZETest(self):
-        self.assertEqual(CAPITALIZE("foo"), "FOO")
-        self.assertEqual(CAPITALIZE("foo"), "FOO")
 
-    def input_value(self):
-        self.assertRaises(TypeError)
-
-
-if __name__ == 'main':
-    unittest.main()
--- test.py
+++
@@ -1,6 +0,0 @@
-import unittest
-
-import main
-
-
-
